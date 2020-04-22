d3a4dfe1beda51e7015eef490d1f6ef5d19fa05f
kartar singh <44725786+KJabandaa10@users.noreply.github.com>
2020-04-22 12:13:02 +0100

    consumer

--- app/src/components/Button.jsx
+++ app/src/components/Button.jsx
@@ -2,11 +2,25 @@ import React from "react";
 import LanguageContext from "../contexts/LanguageContext";
 
 class Button extends React.Component {
-  static contextType = LanguageContext;
+  // static contextType = LanguageContext; Not required when using Consumer
 
   render() {
-    const text = this.context === "english" ? "Submit" : "Voorlegggen";
-    return <button className="ui button primary">{text}</button>;
+    return (
+      // <LanguageContext.Consumer>
+      //   {context => {
+      //     const text = context === "english" ? "Submit" : "Voorleggen";
+      //     return <button className="ui button primary">{text}</button>;
+      //   }}
+      // </LanguageContext.Consumer>
+      //
+      // v^ Either of these are OK (above was my attempt before tutorial)
+      //
+      <button className="ui button primary">
+        <LanguageContext.Consumer>
+          {value => (value === "english" ? "Submit" : "Voorleggen")}
+        </LanguageContext.Consumer>
+      </button>
+    );
   }
 }
 
